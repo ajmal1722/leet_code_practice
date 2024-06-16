4e332d8de91f574675d6dddba5375ee0191bdda6
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-06-16 22:37:31 +0530

    question 3 not aswered

--- app.js
+++ app.js
@@ -85,3 +85,52 @@ var kidsWithCandies = function(candies, extraCandies) {
     return result
 };
 kidsWithCandies([2,3,5,1,3], 3)
+
+
+//  **3.
+
+// Given an array of functions [f1, f2, f3, ..., fn], return a new function fn
+//  that is the function composition of the array of functions.
+
+// The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+
+// The function composition of an empty list of functions is the identity function f(x) = x.
+
+// You may assume each function in the array accepts one integer as input and returns one integer as output.
+
+ 
+
+// Example 1:
+
+// Input: functions = [x => x + 1, x => x * x, x => 2 * x], x = 4
+// Output: 65
+// Explanation:
+// Evaluating from right to left ...
+// Starting with x = 4.
+// 2 * (4) = 8
+// (8) * (8) = 64
+// (64) + 1 = 65
+// Example 2:
+
+// Input: functions = [x => 10 * x, x => 10 * x, x => 10 * x], x = 1
+// Output: 1000
+// Explanation:
+// Evaluating from right to left ...
+// 10 * (1) = 10
+// 10 * (10) = 100
+// 10 * (100) = 1000
+// Example 3:
+
+// Input: functions = [], x = 42
+// Output: 42
+// Explanation:
+// The composition of zero functions is the identity function
+
+// var compose = function(functions) {
+//     let x = 1
+//     return function(x) {
+//         console.log('function:', x)
+//         return functions
+//     }
+// };
+// console.log(compose([x => x + 1, x => 2 * x])(4))
