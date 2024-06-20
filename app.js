5ed6adefb87e42289ee9bae8b96d0d65019d800b
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-06-20 10:15:21 +0530

    6

--- app.js
+++ app.js
@@ -181,7 +181,7 @@ var filter = function(arr, fn) {
     }
     return res
 };
-console.log(filter([0,10,20,30], function greaterThan10(n) { return n > 10; }));
+// console.log(filter([0,10,20,30], function greaterThan10(n) { return n > 10; }));
 
 
 // **5.
@@ -218,3 +218,44 @@ var map = function(arr, fn) {
    } 
    return res
 };
+
+
+// **6. Allow One Function Call
+// Given a function fn, return a new function that is identical to the 
+// original function except that it ensures fn is called at most once.
+
+// The first time the returned function is called, it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+ 
+
+// Example 1:
+
+// Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
+// Output: [{"calls":1,"value":6}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(1, 2, 3); // 6
+// onceFn(2, 3, 6); // undefined, fn was not called
+// Example 2:
+
+// Input: fn = (a,b,c) => (a * b * c), calls = [[5,7,4],[2,3,6],[4,6,8]]
+// Output: [{"calls":1,"value":140}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(5, 7, 4); // 140
+// onceFn(2, 3, 6); // undefined, fn was not called
+// onceFn(4, 6, 8); // undefined, fn was not called
+var once = function(fn) {
+    let call = 0
+    return function(...args){
+            call++
+        if (call === 1) {
+            return fn(...args)
+        } 
+    }
+};
+fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6],[3,4,5]]
+const onceFn = once(fn);
+console.log(onceFn(1, 2, 3))
+console.log(onceFn(1, 2, 3)); // 6
+onceFn(2, 3, 6);
