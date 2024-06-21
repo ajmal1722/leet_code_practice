f6d643915ba3317313c3ee8cd1cb65192943aa2f
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-06-21 17:40:17 +0530

    7

--- app.js
+++ app.js
@@ -258,6 +258,38 @@ var once = function(fn) {
 };
 fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6],[3,4,5]]
 const onceFn = once(fn);
-console.log(onceFn(1, 2, 3))
-console.log(onceFn(1, 2, 3)); // 6
-onceFn(2, 3, 6);
+// console.log(onceFn(1, 2, 3))
+// console.log(onceFn(1, 2, 3));
+// onceFn(2, 3, 6);
+
+
+// **7. Add Two Promises
+// Given two promises promise1 and promise2, return a new promise. promise1 and promise2 
+// will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
+ 
+
+// Example 1:
+
+// Input: 
+// promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20)), 
+// promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60))
+// Output: 7
+// Explanation: The two input promises resolve with the values of 2 and 5 respectively. 
+// The returned promise should resolve with a value of 2 + 5 = 7. The time 
+// the returned promise resolves is not judged for this problem.
+// Example 2:
+
+// Input: 
+// promise1 = new Promise(resolve => setTimeout(() => resolve(10), 50)), 
+// promise2 = new Promise(resolve => setTimeout(() => resolve(-12), 30))
+// Output: -2
+// Explanation: The two input promises resolve with the values of 10 and -12 
+// respectively. The returned promise should resolve with a value of 10 + -12 = -2.
+var addTwoPromises = async function(promise1, promise2) {
+    const val1 = await promise1
+    const val2 = await promise2
+    return val1 + val2
+};
+promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20)), 
+promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60))
+addTwoPromises(promise1, promise2)
